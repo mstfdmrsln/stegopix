15aeb967790cb7e953826a863dd1a7355903f033
Mustafa Demiraslan <mstfdmrsln0668@gmail.com>
2025-11-25 02:20:54 +0300

    Revert "Add CLI support and update documentation"
    This reverts commit d08096c8dc8a4a57a1b3f9472f1c026cbbe46210.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/cli.ts'],
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  shims: true,
 });
